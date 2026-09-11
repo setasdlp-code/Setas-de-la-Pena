a22dce95c2003c2849be08a3858467481ac7ad88
Sebastián Pinzón <spinzonsilva@gmail.com>
2026-09-11 15:10:07 -0500

    feat(setas-os): implementar triaje de bioseguridad, protocolo de contaminacion y cuarentena

--- field-os-simulador/setas-os/sw.js
+++ field-os-simulador/setas-os/sw.js
@@ -19,7 +19,7 @@
  */
 
 // build:cache-version
-const CACHE_VERSION = '60c80cb7870b';
+const CACHE_VERSION = '3e9e11c7059e';
 const CACHE = `setas-os-${CACHE_VERSION}`;
 
 // Lo imprescindible para pintar algo y llegar al gate de autenticación.
